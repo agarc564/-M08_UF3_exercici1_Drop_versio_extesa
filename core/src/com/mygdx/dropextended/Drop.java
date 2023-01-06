6edcddec743e0af74fea95b335ea3d61c7ff680a
agarc564 <70956028+agarc564@users.noreply.github.com>
2023-01-06 10:21:51 +0100

    Primer commit

---
+++ core/src/com/mygdx/dropextended/Drop.java
@@ -0,0 +1,32 @@
+package com.mygdx.dropextended;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Drop extends Game {
+	//variables del joc
+	SpriteBatch batch;
+	public BitmapFont font;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		font = new BitmapFont(); // use libGDX's default Arial font
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		font.dispose();
+	}
+}
---
+++ desktop/src/com/mygdx/dropextended/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.dropextended;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.dropextended.Drop;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("drop_extended");
+		new Lwjgl3Application(new Drop(), config);
+	}
+}
